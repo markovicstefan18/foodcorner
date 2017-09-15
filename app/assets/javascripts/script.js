c2f9fe302b1b1f106bfb0ca449f6eb7f9b300aac
Marković Stefan <markovicstefan@gmail.com>
2017-09-15 14:20:33 +0200

    first commit

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,16 @@
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
+
+$(document).ready(function(){
+  $("#dole").mouseover(function(e){
+    var image = $(e.target).attr("id");
+    if ("dole" !== image) document.getElementById("gore").innerHTML = "<img src=/img/" + image + ".jpeg>";
+  });
+
+
+})
